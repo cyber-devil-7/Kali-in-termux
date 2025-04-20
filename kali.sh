125491ec0ebbabc739f59d760ec5930473a5121e
Cyber <amit.github.2007@gmail.com>
2025-04-21 01:58:48 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,4 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
+pkg install wget -y
+
+termux-setup-storage
 
 # Download the NetHunter installer script
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
